7eccb6924cc9a886f475c9c05bf16e5ecb31e6f0
Brennon Loveless <brennon.loveless@gmail.com>
2020-12-09 21:38:36 -0500

    Day 7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -7,11 +7,11 @@ struct Bag {
     count: i32,
 }
 
-fn can_contain(bags: &HashMap<String, Vec<String>>, bag: String) -> bool {
+fn can_contain(bags: &HashMap<String, Vec<Bag>>, bag: String) -> bool {
     match bags.get(&bag) {
         Some(inner_bags) => {
             for inner_bag in inner_bags {
-                if inner_bag == "shiny gold" || can_contain(bags, inner_bag.to_string()) {
+                if inner_bag.color == "shiny gold" || can_contain(bags, inner_bag.color.clone()) {
                     return true;
                 }
             }
@@ -22,12 +22,28 @@ fn can_contain(bags: &HashMap<String, Vec<String>>, bag: String) -> bool {
     }
 }
 
+fn get_bag_child_count(bags: &HashMap<String, Vec<Bag>>, bag: String) -> i32 {
+    match bags.get(&bag) {
+        Some(inner_bags) => {
+            let mut child_count = 0;
+
+            for inner_bag in inner_bags {
+                let child_bag_count = get_bag_child_count(&bags, inner_bag.color.clone());
+                child_count += inner_bag.count + inner_bag.count * child_bag_count;
+            }
+
+            child_count
+        }
+        None => 0
+    }
+}
+
 fn main() {
     let bag_regex = Regex::new(r"^([0-9]+) ([\w ]*) bags?\.?$").expect("Unable to compile regex");
     let input = fs::read_to_string("input.txt").expect("Unable to read input.txt");
     let lines: Vec<&str> = input.split('\n').collect();
 
-    let mut parent_bags: HashMap<String, Vec<String>> = HashMap::new();
+    let mut parent_bags: HashMap<String, Vec<Bag>> = HashMap::new();
 
     for line in lines {
         let parts: Vec<&str> = line.split(" bags contain ").collect();
@@ -36,11 +52,15 @@ fn main() {
         for inner_bag in inner_bags_raw {
             for cap in bag_regex.captures(inner_bag) {
                 let parent_bag = parent_bags.entry(parts[0].to_string()).or_insert(Vec::new());
-                parent_bag.push(cap[2].to_string());
+                parent_bag.push(Bag {
+                    color: cap[2].to_string(),
+                    count: cap[1].parse().expect("Found an invalid bag count"),
+                });
             }
         }
     }
 
+    // Part 1 --------------------------------------------------------------------------------------
     let mut bags_that_can_contain_shiny_gold = 0;
     for parent_bag in parent_bags.keys() {
         if parent_bag != "shiny gold" {
@@ -50,5 +70,11 @@ fn main() {
         }
     }
 
-    println!("Bags that can contain shiny gold: {}", bags_that_can_contain_shiny_gold);
+    println!("Part 1 - Bags that can contain shiny gold: {}", bags_that_can_contain_shiny_gold);
+
+    // Part 2 --------------------------------------------------------------------------------------
+
+    let child_count = get_bag_child_count(&parent_bags, "shiny gold".to_string());
+
+    println!("Part 2 - Shiny gold children count: {}", child_count);
 }
